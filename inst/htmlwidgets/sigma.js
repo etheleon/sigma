357ecb0d11ceeb75e40a4809ce835256c4ba222a
etheleon <picy2k@gmail.com>
2015-07-12 17:43:57 +0800

    fiddle with resize in javascript binding

--- inst/htmlwidgets/sigma.js
+++ inst/htmlwidgets/sigma.js
@@ -1,30 +1,30 @@
 HTMLWidgets.widget({
 
   name: "sigma",
-  
+
   type: "output",
-  
+
   initialize: function(el, width, height) {
-   
+
     // create our sigma object and bind it to the element
     var sig = new sigma(el.id);
-    
+
     // return it as part of our instance data
     return {
       sig: sig
     };
   },
-  
+
   renderValue: function(el, x, instance) {
-      
+
     // parse gexf data
     var parser = new DOMParser();
     var data = parser.parseFromString(x.data, "application/xml");
-    
+
     // apply settings
     for (var name in x.settings)
       instance.sig.settings(name, x.settings[name]);
-    
+
     // update the sigma instance
     sigma.parsers.gexf(
       data,          // parsed gexf data
@@ -35,11 +35,11 @@ HTMLWidgets.widget({
       }
     );
   },
-  
+
   resize: function(el, width, height, instance) {
-    
+
     // forward resize on to sigma renderers
     for (var name in instance.sig.renderers)
-      instance.sig.renderers[name].resize(width, height);  
+      instance.sig.renderers[name].resize(width);
   }
 });
